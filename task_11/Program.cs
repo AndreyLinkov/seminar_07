d385dc2da392e43c660a95a22f5d30c88d8de72f
Andrey Linkov <linkov.developer@inbox.ru>
2024-03-13 20:37:51 +0300

    add task_11

---
+++ task_11/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+int CheckInput(string message)
+{
+    bool isInputValue = true;
+    int num;
+    do
+    {
+        Console.Write(message);
+        string input = Console.ReadLine()!;
+        if (int.TryParse(input, out num))
+        {
+            isInputValue = false;
+        }
+         else
+            Console.WriteLine("Ошибка ввода!");
+    } while (isInputValue);
+
+    return num;
+}
+
+void Operation(int firstNum, int secondNum)
+{
+    int num = firstNum;
+    Console.Write($"{num} ");
+
+    if (num == secondNum) {
+        return;
+    }
+    else {
+        Operation(firstNum + 1, secondNum);
+    }
+}
+
+int firstNumber = CheckInput("Введите первое число: ");
+int secondNumber = CheckInput("Введите второе число: ");
+
+Operation(firstNumber, secondNumber);
